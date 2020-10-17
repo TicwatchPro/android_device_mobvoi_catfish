8cec12441bf17a9b600a34ff3aec5d9e7787d4d6
TipzTeam <rcheung844@gmail.com>
2020-10-17 19:06:07 +0800

    catfish: proprietary-files: Update scripts
    Signed-off-by: TipzTeam <rcheung844@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 
 set -e
 
-DEVICE=TicWatch Pro
+DEVICE=catfish
 VENDOR=mobvoi
 
 INITIAL_COPYRIGHT_YEAR=2020
@@ -42,8 +42,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 # Copyright headers and guards
 write_headers
 
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
-# We are done!
+# Finish
 write_footers
