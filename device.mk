cbb820e9b31dec6be4d864cf1af7495623f5f551
TipzTeam <rcheung844@gmail.com>
2020-10-17 21:46:06 +0800

    catfish: device: Cleanup
    Signed-off-by: TipzTeam <rcheung844@gmail.com>

--- device.mk
+++ device.mk
@@ -15,6 +15,8 @@
 
 DEVICE_PATH := device/mobvoi/catfish
 
+TARGET_COPY_SYSTEM_ETC := $(TARGET_COPY_OUT_SYSTEM)/etc
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
@@ -25,12 +27,12 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
-    $(DEVICE_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_platform_info.xml \
-    $(DEVICE_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
-    $(DEVICE_PATH)/configs/audio/mixer_paths_msm8909_pm8916.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_msm8909_pm8916.xml \
-    $(DEVICE_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(DEVICE_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(DEVICE_PATH)/configs/audio/audio_effects.conf:$(TARGET_OUT_VENDOR_ETC)/audio_effects.conf \
+    $(DEVICE_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_SYSTEM_ETC)/audio_platform_info.xml \
+    $(DEVICE_PATH)/configs/audio/audio_policy.conf:$(TARGET_OUT_VENDOR_ETC)/audio_policy.conf \
+    $(DEVICE_PATH)/configs/audio/mixer_paths_msm8909_pm8916.xml:$(TARGET_OUT_VENDOR_ETC)/mixer_paths_msm8909_pm8916.xml \
+    $(DEVICE_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_OUT_VENDOR_ETC)/sound_trigger_mixer_paths.xml \
+    $(DEVICE_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_OUT_VENDOR_ETC)/sound_trigger_platform_info.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -58,8 +60,8 @@ PRODUCT_PACKAGES += \
 
 # GPS configurations
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(DEVICE_PATH)/configs/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml
+    $(DEVICE_PATH)/configs/gps/gps.conf:$(TARGET_OUT_VENDOR_ETC)/gps.conf \
+    $(DEVICE_PATH)/configs/gps/gps.xml:$(TARGET_OUT_VENDOR_ETC)/gps.xml
 
 # Keyboard
 PRODUCT_PACKAGES += \
@@ -73,11 +75,11 @@ PRODUCT_COPY_FILES += \
 
 # Media configurations
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml \
-    $(DEVICE_PATH)/configs/media/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_audio.xml \
-    $(DEVICE_PATH)/configs/media/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_telephony.xml \
-    $(DEVICE_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles.xml \
-    $(DEVICE_PATH)/configs/media/media_profiles_V1_0.dtd:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_V1_0.dtd
+    $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_SYSTEM_ETC)/media_codecs.xml \
+    $(DEVICE_PATH)/configs/media/media_codecs_google_audio.xml:$(TARGET_COPY_SYSTEM_ETC)/media_codecs_google_audio.xml \
+    $(DEVICE_PATH)/configs/media/media_codecs_google_telephony.xml:$(TARGET_COPY_SYSTEM_ETC)/media_codecs_google_telephony.xml \
+    $(DEVICE_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_SYSTEM_ETC)/media_profiles.xml \
+    $(DEVICE_PATH)/configs/media/media_profiles_V1_0.dtd:$(TARGET_COPY_SYSTEM_ETC)/media_profiles_V1_0.dtd
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -85,32 +87,37 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-impl \
     libnfc-nci \
     libnfc_nci_jni \
-    NfcNci \
+    NfcNci
+
+# NFC configurations
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_OUT_VENDOR_ETC)/libnfc-hal-st.conf \
+    $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_OUT_VENDOR_ETC)/libnfc-nci.conf \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.audio.output.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.screen.portrait.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.screen.portrait.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.heartrate.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.heartrate.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.stepcounter.xml \
-    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.stepdetector.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.software.connectionservice.xml:(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.connectionservice.xml \
-    frameworks/native/data/etc/wearable_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/wearable_core_hardware.xml \
-    $(DEVICE_PATH)/configs/permissions/com.google.clockwork.hardware.sensor.llob.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.clockwork.hardware.sensor.llob.xml \
-    $(DEVICE_PATH)/configs/permissions/com.google.wearable.hardware.sensor.heartrate.fitness.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.wearable.hardware.sensor.heartrate.fitness.xml
+    frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.audio.output.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.screen.portrait.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.screen.portrait.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.heartrate.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.heartrate.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.connectionservice.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/android.software.connectionservice.xml \
+    frameworks/native/data/etc/wearable_core_hardware.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/wearable_core_hardware.xml \
+    $(DEVICE_PATH)/configs/permissions/com.google.clockwork.hardware.sensor.llob.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/com.google.clockwork.hardware.sensor.llob.xml \
+    $(DEVICE_PATH)/configs/permissions/com.google.wearable.hardware.sensor.heartrate.fitness.xml:$(TARGET_COPY_SYSTEM_ETC)/permissions/com.google.wearable.hardware.sensor.heartrate.fitness.xml
 
 # Rootdir
 PRODUCT_PACKAGES += \
@@ -127,10 +134,10 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi configurations
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal \
-    $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(DEVICE_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
-    $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(DEVICE_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_OUT_VENDOR_ETC)/wifi/bcmdhd.cal \
+    $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_OUT_VENDOR_ETC)/wifi/p2p_supplicant_overlay.conf \
+    $(DEVICE_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_OUT_VENDOR_ETC)/wifi/wpa_supplicant.conf \
+    $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_OUT_VENDOR_ETC)/wifi/wpa_supplicant_overlay.conf \
 
 # System Properties
 $(call inherit-product, device/mobvoi/catfish/system_prop.mk)
