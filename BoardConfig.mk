25b4a1c2e98ded86c7b0b3fda4fe49fe53bebf82
TipzTeam <rcheung844@gmail.com>
2020-10-18 08:27:24 +0800

    catfish: init: Use custom init lib
    Signed-off-by: TipzTeam <rcheung844@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,9 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
+TARGET_LIBINIT_MSM8909_DEFINES_FILE := $(DEVICE_PATH)/init/init_scale.cpp
 TARGET_INIT_VENDOR_LIB := libinit_msm8909
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8909
 
 # Kernel
